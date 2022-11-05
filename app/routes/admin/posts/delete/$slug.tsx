f5093af3f735c194f4bbd517abd1f749b07d769c
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2022-11-04 21:56:29 -0400

    add delete page

--- app/routes/admin/posts/delete/$slug.tsx
+++ app/routes/admin/posts/delete/$slug.tsx
@@ -1,17 +1,45 @@
-import { redirect } from "@remix-run/node";
-import type { LoaderFunction } from "@remix-run/node";
+import type { LoaderFunction, ActionFunction } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import invariant from "tiny-invariant";
+import { useLoaderData, Form } from "@remix-run/react";
 
-import { deletePost } from "~/models/post.server";
+import type { Post } from "~/models/post.server";
+import { getPost, deletePost } from "~/models/post.server";
+
+type LoaderData = { post: Post };
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.slug, `params.slug is required`);
 
-  await deletePost(params.slug);
+  const post = await getPost(params.slug);
+
+  invariant(post, `Post not found: ${params.slug}`);
+
+  return json<LoaderData>({ post });
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const postSlug = formData.get("postSlug");
+
+  invariant(typeof postSlug === "string", "postSlug must be a string");
+
+  await deletePost(postSlug);
 
   return redirect(`/admin/posts`);
 };
 
 export default function PostDelete() {
-  return null;
+  const { post } = useLoaderData<LoaderData>();
+
+  return (
+    <main>
+      <h1>Are you sure that you want to delete this post?</h1>
+      <h2>{post.title}</h2>
+      <Form method="post">
+        <button type="submit">Delete</button>
+        <input type="hidden" name="postSlug" value={post.slug} />
+      </Form>
+    </main>
+  );
 }
