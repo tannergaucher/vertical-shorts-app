2cf268ab2dd7243bf656c1be11beee85cd5dd01d
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-03-28 08:45:32 -0400

    fix login redirect

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -12,6 +12,7 @@ import * as React from "react";
 import { createUserSession, getUserId } from "~/session.server";
 import { verifyLogin } from "~/models/user.server";
 import { safeRedirect, validateEmail } from "~/utils";
+import { Routes } from "~/routes";
 
 export async function loader({ request }: LoaderArgs) {
   const userId = await getUserId(request);
@@ -23,7 +24,7 @@ export async function action({ request }: ActionArgs) {
   const formData = await request.formData();
   const email = formData.get("email");
   const password = formData.get("password");
-  const redirectTo = safeRedirect(formData.get("redirectTo"), "/notes");
+  const redirectTo = safeRedirect(formData.get("redirectTo"), Routes.Admin);
   const remember = formData.get("remember");
 
   if (!validateEmail(email)) {
@@ -72,7 +73,7 @@ export const meta: MetaFunction = () => {
 
 export default function Page() {
   const [searchParams] = useSearchParams();
-  const redirectTo = searchParams.get("redirectTo") || "/notes";
+  const redirectTo = searchParams.get("redirectTo") || Routes.Admin;
   const actionData = useActionData<typeof action>();
   const emailRef = React.useRef<HTMLInputElement>(null);
   const passwordRef = React.useRef<HTMLInputElement>(null);
