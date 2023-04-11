cf5274ec263e0b0567840b074ce566dbc7a82a92
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-04-11 23:06:28 +0200

    Update index.ts

--- cloud-functions/upload-instagram-reel/index.ts
+++ cloud-functions/upload-instagram-reel/index.ts
@@ -1,12 +1,9 @@
-export async function uploadInstagramReel(cloudEvent: any) {
-  // step 1. Create the upload container
-
+export async function uploadInstagramReel() {
   const USER_ID = "111151558605883";
   const IMAGE_URL = `https://storage.googleapis.com/clfsh0ses000ejs08izbzgm5n/jkjkjk.jpg`;
   const CAPTION = `#testCaption`;
 
-  const ACCESS_TOKEN =
-    "EABYZCQTYX4ckBAOnaMPp1u09mZCQBPc6rj7XQlBwsjComxlLo5eN3JsnQjeqtVPLOCJ6nDXFDy3H2nc3KhFAZBM5Smejf2FyH6v8WXDtcugf2CEYyuOIz5qeTMdsBvVRf32hL6TfEmR4Y4ScmAoyGhuXshF91YunZA6ZCC0CVVXia3ZCTxUIdblZC9GEl5qHFQZD";
+  const ACCESS_TOKEN = "";
 
   const res = await fetch(
     `https://graph.facebook.com/v16.0/${USER_ID}/media?image_url=${IMAGE_URL}&caption=${CAPTION}&access_token=${ACCESS_TOKEN}`,
@@ -16,10 +13,6 @@ export async function uploadInstagramReel(cloudEvent: any) {
   );
 
   const data = await res.json();
-
-  console.log(data, "__DATA__");
-
-  // step 2. Publish the upload container
 }
 
-uploadInstagramReel("sd");
+uploadInstagramReel();
