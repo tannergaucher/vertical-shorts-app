1e84320d835f2ede6f0aa6e1c80550673d8db62a
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-05-26 13:22:41 +0300

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Vertical Shorts Platform",
   viewport: "width=device-width,initial-scale=1",
 });
 
