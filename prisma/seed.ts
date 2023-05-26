a6ae87d5d8a7c8dcb407f885ed526068cca37500
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-05-26 12:58:18 +0300

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -24,37 +24,6 @@ async function seed() {
     },
   });
 
-  const project = await prisma.project.create({
-    data: {
-      id: "hr-123",
-      title: "Home Rice",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      user: {
-        connect: {
-          id: user.id,
-        },
-      },
-    },
-  });
-
-  const content = await prisma.content.create({
-    data: {
-      slug: "tteokbokki-in-spicy-rose-sauce-dong-won",
-      title: "Tteokbokki in Spicy Rose Sauce - Dong Won",
-      description: "Sweet, spicy, tasty, convenient",
-      tags: ["dong won", "tteokbokki", "korean food"],
-      published: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      project: {
-        connect: {
-          id: project.id,
-        },
-      },
-    },
-  });
-
   console.log(`Database has been seeded. 🌱`);
 }
 
