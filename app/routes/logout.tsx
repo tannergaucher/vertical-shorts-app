2170ec3951bbfe603070da732c16b88d6254ba88
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-05-26 13:19:34 +0300

    Update logout.tsx

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,12 +1,10 @@
-import type { ActionArgs } from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 
 import { logout } from "~/session.server";
 
-export async function action({ request }: ActionArgs) {
-  return logout(request);
-}
+export async function loader({ request }: LoaderArgs) {
+  await logout(request);
 
-export async function loader() {
   return redirect("/");
 }
