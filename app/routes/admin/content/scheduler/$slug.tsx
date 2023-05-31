3be15ac1078896b8f1f8a8bde17dddda3ac80512
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-05-31 09:56:17 +0300

    Update $slug.tsx

--- app/routes/admin/content/scheduler/$slug.tsx
+++ app/routes/admin/content/scheduler/$slug.tsx
@@ -4,6 +4,7 @@ export default function Page() {
       {/* input type date */}
       {/* input type time */}
       {/* default to publishing in 1 hour to allow 4k processing */}
+      {/* and display checkbox component for selecting which social platforms to post to */}
     </main>
   );
 }
