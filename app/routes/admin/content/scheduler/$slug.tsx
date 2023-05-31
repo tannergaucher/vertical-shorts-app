293acfa9e442e61e81a6e1950b3986bcedfcad94
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-05-31 09:21:54 +0300

    Create $slug.tsx

---
+++ app/routes/admin/content/scheduler/$slug.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      {/* input type date */}
+      {/* input type time */}
+      {/* default to publishing in 1 hour to allow 4k processing */}
+    </main>
+  );
+}
