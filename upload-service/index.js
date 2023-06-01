1d178af4b61a195501c937f7cc87a4795b22638d
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-06-01 23:30:14 +0300

    handle uploading to tiktok in chunks

--- upload-service/index.js
+++ upload-service/index.js
@@ -179,6 +179,136 @@ app.post("/upload-youtube-short", async (req, res) => {
     });
 });
 
+app.post("/upload-tiktok", async (req, res) => {
+  const { projectId, slug } = req.body;
+
+  const project = await prisma.project.findUnique({
+    where: {
+      id: projectId,
+    },
+    select: {
+      tikTokCredentials: true,
+    },
+  });
+
+  if (!project.tikTokCredentials) {
+    throw new Error("no tiktok credentials");
+  }
+
+  const videoFilePath = `${slug}.mp4`;
+
+  const videoStats = fs.statSync(videoFilePath);
+  const videoBinary = fs.readFileSync(videoFilePath);
+
+  const videoSize = videoBinary.length;
+  const minChunkSize = 5 * 1024 * 1024; // 5 MB in bytes
+  const maxChunkSize = 64 * 1024 * 1024; // 64 MB in bytes
+  const maxFinalChunkSize = 128 * 1024 * 1024; // 128 MB in bytes
+  const totalChunkCount = Math.ceil(videoSize / maxChunkSize);
+  const chunkSize = Math.floor(videoSize / totalChunkCount);
+
+  const getVideoChunks = () => {
+    if (videoSize < minChunkSize) {
+      // Upload the entire video as a single chunk
+      return [videoBinary];
+    } else if (videoSize <= maxChunkSize) {
+      // Upload the entire video as a single chunk, since it's less than or equal to the maximum chunk size
+      return [videoBinary];
+    } else {
+      const chunks = [];
+      let remainingBytes = videoSize;
+      let offset = 0;
+
+      for (let i = 0; i < totalChunkCount; i++) {
+        let currentChunkSize = chunkSize;
+
+        if (i === totalChunkCount - 1) {
+          // Last chunk can be greater to accommodate any trailing bytes
+          currentChunkSize = Math.min(remainingBytes, maxFinalChunkSize);
+        }
+
+        const chunk = videoBinary.slice(offset, offset + currentChunkSize);
+        chunks.push(chunk);
+
+        offset += currentChunkSize;
+        remainingBytes -= currentChunkSize;
+      }
+
+      return chunks;
+    }
+  };
+
+  const videoChunks = getVideoChunks();
+
+  try {
+    const initRes = await fetch(
+      `https://open.tiktokapis.com/v2/post/publish/inbox/video/init/`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${project.tikTokCredentials.accessToken}`,
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({
+          source: "FILE_UPLOAD",
+          video_size: videoStats.size,
+          chunk_size: chunkSize,
+          total_chunk_count: totalChunkCount,
+        }),
+      }
+    );
+
+    if (!initRes.ok) {
+      console.log(initRes, "initRes.error");
+      // get response body
+      const initResJson = await initRes.json();
+
+      console.log(initResJson, "initResJson");
+
+      throw new Error("ERROR_INITIALIZING_TIKTOK_UPLOAD");
+    }
+
+    const { data } = await initRes.json();
+
+    const { upload_url, publish_id } = data;
+
+    console.log(upload_url, "upload_url");
+    console.log(publish_id, "publish_id");
+
+    await Promise.all(
+      videoChunks.map(async (chunk, index) => {
+        const headers = {
+          "Content-Type": "video/mp4",
+          "Content-Length": chunk.length,
+          "Content-Range": `bytes ${index * chunkSize}-${
+            index * chunkSize + chunk.length - 1
+          }/${videoSize}`,
+        };
+
+        const chunkUploadRes = await fetch(upload_url, {
+          method: "PUT",
+          headers,
+          body: chunk,
+        });
+
+        if (!chunkUploadRes.ok) {
+          console.log(chunkUploadRes, "chunkUploadRes error");
+          throw new Error("ERROR_UPLOADING_TIKTOK_CHUNK");
+        }
+
+        const json = await chunkUploadRes.json();
+        console.log(json, "chunkUploadRes json");
+
+        return chunkUploadRes;
+      })
+    );
+
+    // call check titkok upload status endpoint with publish_id
+  } catch (error) {
+    console.log(error, "error initializing tiktok upload");
+  }
+});
+
 const port = parseInt(process.env.PORT) || 8080;
 
 app.listen(port, () => {
