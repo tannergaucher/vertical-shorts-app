aaaca3c579bfbdec00d6fc0caafed4b02fd6e4fd
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-06-22 13:59:15 +0300

    add initial signup page, stripe cards

--- app/root.tsx
+++ app/root.tsx
@@ -51,6 +51,7 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
       </head>
       <body>
         <nav className={styles.nav}>
