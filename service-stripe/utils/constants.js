0eb8864462e55edddddc7b08d29ab779361ad069
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-06-22 17:51:51 +0300

    Update constants.js

--- service-stripe/utils/constants.js
+++ service-stripe/utils/constants.js
@@ -9,4 +9,4 @@ export const STRIPE_PRODUCTS = {
 export const SERVICE_STRIPE_BASE_URL =
   process.env.NODE_ENV === "production"
     ? `https://service-stripe-yzmezs2csa-ue.a.run.app`
-    : `http://localhost:8000`;
+    : `http://localhost:8080`;
