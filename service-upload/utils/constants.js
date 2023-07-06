038022f52d1e56573959024ff614597b3e3be6b0
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-07-06 16:14:11 +0300

    remove script

---
+++ service-upload/utils/constants.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.APP_BASE_URL = exports.UPLOAD_SERVICE_BASE_URL = void 0;
+exports.UPLOAD_SERVICE_BASE_URL = process.env.NODE_ENV === "production"
+    ? `https://service-upload-yzmezs2csa-ue.a.run.app`
+    : `http://localhost:8080`;
+exports.APP_BASE_URL = process.env.NODE_ENV === "production"
+    ? `https://verticalshorts.app`
+    : `http://localhost:3000`;
