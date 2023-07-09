efbe9c28354c4b6f897b65811ba70d620d874f89
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-07-09 16:03:35 +0300

    Redirect resource route to cloud storage

--- app/routes/resource/serve-video.$projectId.$slug.ts
+++ app/routes/resource/serve-video.$projectId.$slug.ts
@@ -1,4 +1,4 @@
-import type { LoaderArgs } from "@remix-run/node";
+import { type LoaderArgs, redirect } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
 export async function loader({ params }: LoaderArgs) {
@@ -7,20 +7,5 @@ export async function loader({ params }: LoaderArgs) {
   invariant(projectId, "projectId is required");
   invariant(slug, "slug is required");
 
-  const videoRes = await fetch(
-    `https://storage.googleapis.com/${projectId}/${slug}.mp4`
-  );
-
-  if (!videoRes.ok) {
-    return new Response("No video response", {
-      status: 404,
-    });
-  }
-
-  return new Response(videoRes.body, {
-    status: 200,
-    headers: {
-      "Content-Type": "video/mp4",
-    },
-  });
+  return redirect(`https://storage.googleapis.com/${projectId}/${slug}.mp4`);
 }
