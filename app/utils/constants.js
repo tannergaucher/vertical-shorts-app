94cfe1f47edbd81aa88d7810a53da180bec19bd5
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-07-26 15:49:45 +0300

    fix typo

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -4,7 +4,6 @@ export const UPLOAD_SERVICE_BASE_URL =
     : `http://localhost:8080`;
 
 export const CLOUD_VIDEO_INTELLIGENCE_BASE_URL =
-  process.env.NODE_ENV ===
-  `https://service-cloud-video-intelligence-yzmezs2csa-ue.a.run.app`
-    ? ``
+  process.env.NODE_ENV === "production"
+    ? `https://service-cloud-video-intelligence-yzmezs2csa-ue.a.run.app`
     : `http://localhost:8080`;
