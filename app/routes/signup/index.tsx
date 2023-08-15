358ac93e79fb072b8fd9704a98119f05a5a3ccc5
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-08-15 11:57:08 -0400

    load stripe script imperatively

--- app/routes/signup/index.tsx
+++ app/routes/signup/index.tsx
@@ -1,6 +1,7 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { useEffect } from "react";
 
 import { Routes } from "~/routes";
 import { getUser } from "~/session.server";
@@ -36,6 +37,17 @@ export const loader = async ({ request }: LoaderArgs) => {
 export default function Page() {
   const { userId } = useLoaderData<LoaderData>();
 
+  useEffect(() => {
+    const scriptTag = document.createElement("script");
+    scriptTag.src = "https://js.stripe.com/v3/pricing-table.js";
+    scriptTag.async = true;
+    document.body.appendChild(scriptTag);
+
+    return () => {
+      document.body.removeChild(scriptTag);
+    };
+  }, []);
+
   return (
     <main className={styles.main}>
       <h1 className={styles.heading}> Choose Your Plan!</h1>
@@ -44,7 +56,6 @@ export default function Page() {
         publishable-key="pk_test_51NLh4NKQkHgqj5P6rhGx8THLGek4w6jRvFI0MqZ3XxzsWE9U0zEhfi0H84V8DNzYUs0cwx9I35IZKVIsYrZcRH4M00GfghuOVd"
         client-reference-id={userId}
       ></stripe-pricing-table>
-      <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
     </main>
   );
 }
