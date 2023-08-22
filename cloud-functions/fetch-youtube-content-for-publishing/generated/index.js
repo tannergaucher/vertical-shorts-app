735a677aa3a7c07ef38059992370cf58c6171a2b
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-08-21 22:21:01 -0400

    init update

--- cloud-functions/fetch-youtube-content-for-publishing/generated/index.js
+++ cloud-functions/fetch-youtube-content-for-publishing/generated/index.js
@@ -260,7 +260,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/tg/Documents/GitHub/remix-blog/cloud-functions/fetch-youtube-content-for-publishing/generated",
+      "value": "/Users/tg/Documents/vertical-shorts-app/cloud-functions/fetch-youtube-content-for-publishing/generated",
       "fromEnvVar": null
     },
     "config": {
@@ -285,8 +285,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../../prisma",
   "clientVersion": "4.15.0",
@@ -296,14 +295,14 @@ const config = {
   ],
   "activeProvider": "postgresql",
   "dataProxy": false,
-  "postinstall": false
+  "postinstall": true
 }
 
 const fs = require('fs')
 
 config.dirname = __dirname
 if (!fs.existsSync(path.join(__dirname, 'schema.prisma'))) {
-  config.dirname = path.join(process.cwd(), "cloud-functions/fetch-youtube-content-for-publishing/generated")
+  config.dirname = path.join(process.cwd(), "../cloud-functions/fetch-youtube-content-for-publishing/generated")
   config.isBundled = true
 }
 
@@ -327,15 +326,15 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
-path.join(process.cwd(), "cloud-functions/fetch-youtube-content-for-publishing/generated/libquery_engine-darwin-arm64.dylib.node")
+path.join(process.cwd(), "../cloud-functions/fetch-youtube-content-for-publishing/generated/libquery_engine-darwin-arm64.dylib.node")
 
 path.join(__dirname, "libquery_engine-debian-openssl-1.1.x.so.node");
-path.join(process.cwd(), "cloud-functions/fetch-youtube-content-for-publishing/generated/libquery_engine-debian-openssl-1.1.x.so.node")
+path.join(process.cwd(), "../cloud-functions/fetch-youtube-content-for-publishing/generated/libquery_engine-debian-openssl-1.1.x.so.node")
 
 path.join(__dirname, "libquery_engine-debian-openssl-3.0.x.so.node");
-path.join(process.cwd(), "cloud-functions/fetch-youtube-content-for-publishing/generated/libquery_engine-debian-openssl-3.0.x.so.node")
+path.join(process.cwd(), "../cloud-functions/fetch-youtube-content-for-publishing/generated/libquery_engine-debian-openssl-3.0.x.so.node")
 
 path.join(__dirname, "libquery_engine-rhel-openssl-1.0.x.so.node");
-path.join(process.cwd(), "cloud-functions/fetch-youtube-content-for-publishing/generated/libquery_engine-rhel-openssl-1.0.x.so.node")
+path.join(process.cwd(), "../cloud-functions/fetch-youtube-content-for-publishing/generated/libquery_engine-rhel-openssl-1.0.x.so.node")
 path.join(__dirname, "schema.prisma");
-path.join(process.cwd(), "cloud-functions/fetch-youtube-content-for-publishing/generated/schema.prisma")
+path.join(process.cwd(), "../cloud-functions/fetch-youtube-content-for-publishing/generated/schema.prisma")
--- service-stripe/generated/index.js
+++ service-stripe/generated/index.js
@@ -260,7 +260,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/tg/Documents/GitHub/remix-blog/service-stripe/generated",
+      "value": "/Users/tg/Documents/vertical-shorts-app/service-stripe/generated",
       "fromEnvVar": null
     },
     "config": {
@@ -276,8 +276,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../prisma",
   "clientVersion": "4.15.0",
@@ -287,14 +286,14 @@ const config = {
   ],
   "activeProvider": "postgresql",
   "dataProxy": false,
-  "postinstall": false
+  "postinstall": true
 }
 
 const fs = require('fs')
 
 config.dirname = __dirname
 if (!fs.existsSync(path.join(__dirname, 'schema.prisma'))) {
-  config.dirname = path.join(process.cwd(), "service-stripe/generated")
+  config.dirname = path.join(process.cwd(), "../service-stripe/generated")
   config.isBundled = true
 }
 
@@ -318,15 +317,15 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
-path.join(process.cwd(), "service-stripe/generated/libquery_engine-darwin-arm64.dylib.node")
+path.join(process.cwd(), "../service-stripe/generated/libquery_engine-darwin-arm64.dylib.node")
 
 path.join(__dirname, "libquery_engine-debian-openssl-1.1.x.so.node");
-path.join(process.cwd(), "service-stripe/generated/libquery_engine-debian-openssl-1.1.x.so.node")
+path.join(process.cwd(), "../service-stripe/generated/libquery_engine-debian-openssl-1.1.x.so.node")
 
 path.join(__dirname, "libquery_engine-debian-openssl-3.0.x.so.node");
-path.join(process.cwd(), "service-stripe/generated/libquery_engine-debian-openssl-3.0.x.so.node")
+path.join(process.cwd(), "../service-stripe/generated/libquery_engine-debian-openssl-3.0.x.so.node")
 
 path.join(__dirname, "libquery_engine-rhel-openssl-1.0.x.so.node");
-path.join(process.cwd(), "service-stripe/generated/libquery_engine-rhel-openssl-1.0.x.so.node")
+path.join(process.cwd(), "../service-stripe/generated/libquery_engine-rhel-openssl-1.0.x.so.node")
 path.join(__dirname, "schema.prisma");
-path.join(process.cwd(), "service-stripe/generated/schema.prisma")
+path.join(process.cwd(), "../service-stripe/generated/schema.prisma")
