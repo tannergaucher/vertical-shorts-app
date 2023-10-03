389b3f3ac8127facb909dc84249184e11e4928a2
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-10-03 18:09:36 -0400

    Update upload-tiktok.ts

--- service-upload/functions/upload-tiktok.ts
+++ service-upload/functions/upload-tiktok.ts
@@ -34,64 +34,55 @@ export async function uploadTikTok(
     throw new Error("no tiktok credentials");
   }
 
-  try {
-    res.status(200).send("Starting upload to tiktok");
+  console.log(`Starting upload to tiktok for ${projectId} / ${slug}`);
 
-    const initResponse = await fetch(
-      `https://open.tiktokapis.com/v2/post/publish/inbox/video/init/`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${project.tikTokCredentials.accessToken}`,
-          "Content-Type": "application/json;",
-        },
-        body: JSON.stringify({
-          source: "PULL_FROM_URL",
-          video_url: `${APP_BASE_URL}/resource/serve-video/${projectId}/${slug}`, // "https://sf16-va.tiktokcdn.com/obj/eden-va2/uvpapzpbxjH-aulauvJ-WV[[/ljhwZthlaukjlkulzlp/3min.mp4",
-        }),
-      }
-    );
-
-    if (!initResponse.ok) {
-      const initResponseError: UploadTikTokResponse = {
-        success: false,
-        message: "Error initializing tiktok upload",
-      };
-
-      return res.status(500).json(initResponseError);
+  const initResponse = await fetch(
+    `https://open.tiktokapis.com/v2/post/publish/inbox/video/init/`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${project.tikTokCredentials.accessToken}`,
+        "Content-Type": "application/json;",
+      },
+      body: JSON.stringify({
+        source: "PULL_FROM_URL",
+        video_url: `${APP_BASE_URL}/resource/serve-video/${projectId}/${slug}`, // "https://sf16-va.tiktokcdn.com/obj/eden-va2/uvpapzpbxjH-aulauvJ-WV[[/ljhwZthlaukjlkulzlp/3min.mp4",
+      }),
     }
+  );
 
-    const { data } = (await initResponse.json()) as {
-      data: {
-        publish_id: string;
-      };
-    };
-
-    await prisma.content.update({
-      where: {
-        projectId_slug: {
-          projectId,
-          slug,
-        },
-      },
-      data: {
-        tikTokStatus: UploadStatus.UPLOADING,
-        tikTokId: data.publish_id,
-      },
-    });
-
-    const response: UploadTikTokResponse = {
-      success: true,
-      message: "Successfully initialized tiktok upload",
-    };
-
-    return res.json(response);
-  } catch (error) {
-    const errorResponse: UploadTikTokResponse = {
+  if (!initResponse.ok) {
+    const initResponseError: UploadTikTokResponse = {
       success: false,
       message: "Error initializing tiktok upload",
     };
 
-    return res.status(500).json(errorResponse);
+    return res.status(500).json(initResponseError);
   }
+
+  const { data } = (await initResponse.json()) as {
+    data: {
+      publish_id: string;
+    };
+  };
+
+  await prisma.content.update({
+    where: {
+      projectId_slug: {
+        projectId,
+        slug,
+      },
+    },
+    data: {
+      tikTokStatus: UploadStatus.UPLOADING,
+      tikTokId: data.publish_id,
+    },
+  });
+
+  const response: UploadTikTokResponse = {
+    success: true,
+    message: "Successfully initialized tiktok upload",
+  };
+
+  return res.status(200).json(response);
 }
